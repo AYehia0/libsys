633de9310e6ba9253f0caaf8634bf0699f4f81f6
AYehia0 <ahmedyehia611@gmail.com>
2024-01-14 03:37:58 +0200

    SSL configurations for production

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
+import fs from "fs";
+import https from "https";
 import app from "./app";
 import database from "./database";
 
 const port = parseInt(process.env.PORT as string, 10) || 3000;
 
-app.listen(port, () => {
-    console.log(
-        `Listening on port ${port}, running on: ${process.env.NODE_ENV}`,
-    );
-    console.log("Running db migrations...");
-    database.runMigrations();
-});
+if (process.env.NODE_ENV === "production") {
+    const sslConfig = {
+        key: fs.readFileSync(
+            "/etc/letsencrypt/live/yourdomain.com/privkey.pem",
+        ),
+        cert: fs.readFileSync(
+            "/etc/letsencrypt/live/yourdomain.com/fullchain.pem",
+        ),
+    };
+    https.createServer(sslConfig, app).listen(process.env.PORT);
+} else {
+    app.listen(port, () => {
+        console.log(
+            `Listening on port ${port}, running on: ${process.env.NODE_ENV}`,
+        );
+        console.log("Running db migrations...");
+        database.runMigrations();
+    });
+}
