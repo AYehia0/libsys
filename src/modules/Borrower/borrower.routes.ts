41595e8f199364764907d168256d8a85ec3e38a3
AYehia0 <ahmedyehia611@gmail.com>
2024-01-14 04:08:02 +0200

    adding caches for specific routes only: GET

--- src/modules/Borrower/borrower.routes.ts
+++ src/modules/Borrower/borrower.routes.ts
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import * as BorrowerController from "./borrower.controller";
+import * as cache from "route-cache";
 
 // create the book Router
 const router = Router();
 
 router.post("/register", BorrowerController.registerBorrower);
-router.get("/:id/books", BorrowerController.getBorrowedBooks);
-router.get("/", BorrowerController.getAllBorrowers);
+router.get(
+    "/:id/books",
+    cache.cacheSeconds(10),
+    BorrowerController.getBorrowedBooks,
+);
+router.get("/", cache.cacheSeconds(10), BorrowerController.getAllBorrowers);
 router.patch("/:id", BorrowerController.updateBorrower);
-router.get("/:id", BorrowerController.getBorrowerById);
+router.get("/:id", cache.cacheSeconds(10), BorrowerController.getBorrowerById);
 router.delete("/:id", BorrowerController.deleteBorrowerById);
 
 export default router;
