064541b3bb92b88b3693103ecd4e8ede72cac2ae
AYehia0 <ahmedyehia611@gmail.com>
2024-01-17 02:47:22 +0200

    debug: ok, last thing for today, wait-for-it.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,13 @@
 # tests in a test database. It is used by the CI system.
 
 # run the dev docker-compose file with .env.test
-docker-compose -f docker-compose.test.yml --env-file .test.env up -d --build --wait --wait-timeout 30
+docker-compose -f docker-compose.test.yml --env-file .test.env up -d --build
+
+echo "Waiting for the database to be ready..."
+./wait-for-it.sh -t 0 db:5432
 
 # run the tests
+echo "Running tests..."
 npm run test
 
 # # stop the dev docker-compose file and clean up 
