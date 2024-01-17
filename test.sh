f43aad8f876f9169d42dafb999853faf28026fc1
AYehia0 <ahmedyehia611@gmail.com>
2024-01-17 02:12:22 +0200

    debug: showing logs for docker-compose

--- test.sh
+++ test.sh
@@ -4,10 +4,12 @@
 # tests in a test database. It is used by the CI system.
 
 # run the dev docker-compose file with .env.test
-docker-compose -f docker-compose.test.yml --env-file .test.env up -d --build && docker-compose -f docker-compose.test.yml logs -f
+docker-compose -f docker-compose.test.yml --env-file .test.env up -d --build
 
 # run the tests
 npm run test
+
+docker-compose -f docker-compose.test.yml logs -f
 
 # # stop the dev docker-compose file and clean up 
 docker-compose -f docker-compose.test.yml down -v
