acefc41ebc06f8c9072d8c8507e29e7fa3607a01
AYehia0 <ahmedyehia611@gmail.com>
2024-01-17 02:35:31 +0200

    wait for the container to fire up

--- test.sh
+++ test.sh
@@ -4,12 +4,16 @@
 # tests in a test database. It is used by the CI system.
 
 # run the dev docker-compose file with .env.test
-docker-compose -f docker-compose.test.yml --env-file .test.env up --build
+docker-compose -f docker-compose.test.yml --env-file .test.env up -d --build
+
+# wait for PostgreSQL to be ready inside the container
+until docker-compose -f docker-compose.test.yml exec -T db pg_isready -h localhost -p 5432; do
+  >&2 echo "Postgres is not ready - sleeping"
+  sleep 1
+done
 
 # run the tests
 npm run test
-
-docker-compose -f docker-compose.test.yml logs -f
 
 # # stop the dev docker-compose file and clean up 
 docker-compose -f docker-compose.test.yml down -v
